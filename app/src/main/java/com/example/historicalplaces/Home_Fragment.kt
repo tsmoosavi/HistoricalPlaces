55962b225d2295cde0eee8e8d311b51c5f2ec9c1
ts moosavi <tt.musavi@gmail.com>
2022-03-17 00:12:56 +0330

    four historical places added to home

--- app/src/main/java/com/example/historicalplaces/Home_Fragment.kt
+++ app/src/main/java/com/example/historicalplaces/Home_Fragment.kt
@@ -28,7 +28,44 @@ class Home_Fragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         firstPlace()
+        secondPlace()
+        thirdPlace()
+        forthPlace()
 
+
+    }
+
+    private fun forthPlace() {
+        context?.let {
+            Glide.with(it)
+                .load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQohoAxiP5fZ4qURf4wcTcjkHIgzb_U4DxMWw&usqp=CAU")
+                .into(binding.image4)
+        }
+        binding.ll4.setOnClickListener{
+            findNavController().navigate(R.id.action_home_Fragment_to_detailFragment)
+        }
+    }
+
+    private fun thirdPlace() {
+        context?.let {
+            Glide.with(it)
+                .load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoAez9VXW5YqYe6vlAILHRKHw4ajkU5qjdQ&usqp=CAU")
+                .into(binding.image3)
+        }
+        binding.ll3.setOnClickListener{
+            findNavController().navigate(R.id.action_home_Fragment_to_detailFragment)
+        }
+    }
+
+    private fun secondPlace() {
+        context?.let {
+            Glide.with(it)
+                .load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPWpEtc4bJm2AmjD9ZY9zEtRz5YXJxB-voZQ&usqp=CAU")
+                .into(binding.image2)
+        }
+        binding.ll2.setOnClickListener{
+            findNavController().navigate(R.id.action_home_Fragment_to_detailFragment)
+        }
     }
 
     private fun firstPlace() {
