a8926bc80cca78171c5a04474a9b371d62c06f11
ts moosavi <tt.musavi@gmail.com>
2022-03-16 22:18:53 +0330

    Initial commit

---
+++ app/src/main/java/com/example/historicalplaces/DetailFragment.kt
@@ -0,0 +1,26 @@
+package com.example.historicalplaces
+
+import android.os.Binder
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.historicalplaces.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+lateinit var binding: FragmentDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding =  FragmentDetailBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/historicalplaces/SettingFragment.kt
@@ -0,0 +1,26 @@
+package com.example.historicalplaces
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.historicalplaces.databinding.FragmentSettingBinding
+
+class SettingFragment : Fragment() {
+lateinit var binding: FragmentSettingBinding
+override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingBinding.inflate(inflater,container,false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
