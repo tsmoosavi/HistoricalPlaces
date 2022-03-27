392aff5fa57d7483bccdaf90798d6362841d4990
ts moosavi <tt.musavi@gmail.com>
2022-03-27 23:50:23 +0430

    BankAccountInfoFragment.kt and it's binding added

---
+++ app/src/main/java/com/example/historicalplaces/BankAccountInfoFragment.kt
@@ -0,0 +1,27 @@
+package com.example.historicalplaces
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.historicalplaces.databinding.FragmentBankAccountInfoBinding
+
+
+class BankAccountInfoFragment : Fragment() {
+    lateinit var binding: FragmentBankAccountInfoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBankAccountInfoBinding.inflate(inflater,container,false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
